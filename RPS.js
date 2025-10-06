99b655b38d2bcd7a76753315b77a002b2ffc43e8
Brian Zhang <zhang.15977@osu.edu>
2025-10-06 12:06:56 -0400

    Add Computer Choice
    used a random number generator to get
    the computer's choice of rock, paper, or
    scissor

---
+++ RPS.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    let choice =  (Math.random() * 3);
+    
+   if (choice < 1){
+        return 'rock'
+   } else if (choice < 2) {
+        return 'paper'
+    } else {
+       return 'scissors'
+   }
+}
+
+console.log(getComputerChoice())
