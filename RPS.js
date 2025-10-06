e4cf34870b79128ad31f73ae6372cfd775990e7b
Brian Zhang <zhang.15977@osu.edu>
2025-10-06 13:01:29 -0400

    Implement playRound
    added a method that plays a round of
    rock, paper, scissors, and increments
    the scores of the player or cpu,
    depending on who wins.

--- RPS.js
+++ RPS.js
@@ -11,8 +11,37 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let choice = prompt('choose rock, paper, or scissor')
+    let choice = prompt('choose rock, paper, or scissors')
     return choice
 }
-console.log(getComputerChoice())
-console.log(getHumanChoice());
+
+function playRound(human, cpu) {
+    if (human == 'paper' && cpu == 'scissor') {
+        console.log("You lose! scissors beat paper")
+        cpuScore++
+    } else if (human == 'paper' && cpu == 'rock') {
+        console.log("You win! paper beats rock")
+        humanScore++
+    } else if (human == 'rock' && cpu == 'paper') {
+        console.log("You lose! paper beats rock")
+        cpuScore++
+    } else if (human == 'rock' && cpu == 'scissor') {
+        console.log("You win! rock beats scissor")
+        humanScore++
+    } else if (human == 'scissor' && cpu == 'paper') {
+        console.log("You win! scissors beat paper")
+        humanScore++
+    } else if (human == 'scissor' && cpu == 'rock') {
+        console.log("You lose! rock beats scissor")
+        cpuScore++
+    } else {
+        console.log("Its a tie!")
+    }
+}
+
+let humanScore = 0
+let cpuScore = 0
+const humanChoice = getHumanChoice().toLowerCase()
+const cpuChoice = getComputerChoice()
+
+playRound(humanChoice,cpuChoice)
